0c7fa3b3d24bed8cb4399ca3034f3efcd4bfe950
Genesis Maria Nobrega Betencourt <78150653+GenesisDN@users.noreply.github.com>
2024-03-20 20:15:23 +0000

    throw bone

--- Assets/Oculus Hands/ActivateTeleportationRay.cs
+++ Assets/Oculus Hands/ActivateTeleportationRay.cs
@@ -16,5 +16,8 @@ public class ActivateTeleportationRay : MonoBehaviour
     {
         leftTeleportation.SetActive(leftActivate.action.ReadValue<float>() > 0.1f);
         rightTeleportation.SetActive(rightActivate.action.ReadValue<float>() > 0.1f);
+
     }
+
+    
 }
