6d538d95f5670a40e237931c49bf6fce1bc176c1
Genesis Maria Nobrega Betencourt <78150653+GenesisDN@users.noreply.github.com>
2024-03-20 21:40:47 +0000

    grab ray

--- Assets/Oculus Hands/AnimateHandsOnInput.cs
+++ Assets/Oculus Hands/AnimateHandsOnInput.cs
@@ -2,27 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class AnimateHandOnInput : MonoBehaviour
 {
     public InputActionProperty pinchAnimationAction;
     public InputActionProperty gripAnimationAction;
     public Animator handAnimator;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+
+
     void Update()
     {
         float triggerValue = pinchAnimationAction.action.ReadValue<float>();
         float gripValue = gripAnimationAction.action.ReadValue<float>();
 
+
         handAnimator.SetFloat("Trigger", triggerValue);
         handAnimator.SetFloat("Grip", gripValue);
-
-        Debug.Log(triggerValue);
     }
 }
